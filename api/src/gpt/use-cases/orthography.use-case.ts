abb3cdb6d046d4c4051588429079882c2f29f3de
juan-pablo-perez-up-co <130450324+juan-pablo-perez-up-co@users.noreply.github.com>
2024-10-20 20:03:09 +0100

    First version of the API server

---
+++ api/src/gpt/use-cases/orthography.use-case.ts
@@ -0,0 +1,61 @@
+import { Logger } from '@nestjs/common';
+import OpenAI from 'openai';
+
+interface Options {
+  prompt: string;
+}
+
+export const orthographyCheckUseCase = async (openai: OpenAI, options: Options) => {
+
+  const logger = new Logger(orthographyCheckUseCase.name);
+
+  const { prompt } = options;
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `
+        Te serán proveídos textos en español con posibles errores ortográficos y gramaticales,
+        Las palabras usadas deben de existir en el diccionario de la Real Academia Española,
+        Debes de responder en formato JSON, 
+        tu tarea es corregirlos y retornar información soluciones, 
+        también debes de dar un porcentaje de acierto por el usuario,
+        
+        Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+        Ejemplo de salida:
+        {
+          userScore: number,
+          errors: string[], // ['error -> solución']
+          message: string, //  Usa emojis y texto para felicitar al usuario
+        }
+        `
+      },
+      {
+        role: 'user',
+        content: prompt,
+      }
+    ],
+    // model: "gpt-3.5-turbo-1106",
+    model: "gpt-4o",
+    temperature: 0.3,
+    max_tokens: 2048,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    response_format: {
+      type: 'json_object'
+    }
+  });
+
+  logger.debug(`Completion: ${JSON.stringify(completion)}`);
+  const content = completion.choices[0].message.content;
+  if (content === null) {
+    throw new Error("Completion content is null");
+  }
+  const jsonResp = JSON.parse(content);
+
+  return jsonResp;
+
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );  
+  app.enableCors();
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+  });  
 }
 bootstrap();
